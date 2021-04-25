3ffb527e83e400ecad43bc3aa892a88f6df69af0
Ege Meriç Erdoğan <egemeric99@gmail.com>
2021-04-25 14:58:04 +0300

    web development

--- webapp/html/js/index.js
+++ webapp/html/js/index.js
@@ -1,6 +1,23 @@
 function GetAllDoms(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET","http://127.0.0.1:9000/api/get/getalldomains",false);
-    xhttp.send(null);
-    return xhttp.responseText;
+    $.getJSON( "/api/get/getalldomains", function( data ) {
+        var items = [];
+        $.each( data, function(key, val ) {
+            items.push("<div class='row-sm-3 shadow-none p-3 mb-5 bg-light rounded'>"+key+") <b>" + val.Name +"</b><br>Uuid:"+val.Uuid+"<br>State:"+val.Status+"</div>" );
+            console.log(key)
+        });
+        
+        $( "<div/>", {
+            "class": "col",
+            html: items.join( "" )
+        }).appendTo( ".container-fluid" );
+        });
+}
+
+function HostMemInfo(){
+    var value= $.ajax({
+        dataType: "json", 
+        url: '/api/get/hostmeminfo', 
+        async: false
+     });
+     return value;
 }
